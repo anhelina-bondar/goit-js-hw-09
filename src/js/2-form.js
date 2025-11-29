409a88cb048aa3b127baaf4b3877e48ca8d4b542
Anhelina Bondar <angelina072017@gmail.com>
2025-11-29 14:56:04 +0100

    add task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,35 @@
-console.log('Hell!!o');
+let formData = { email: '', message: '' };
+const formElem = document.querySelector('.feedback-form');
+
+const savedData = localStorage.getItem('feedback-form-state');
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formElem.email.value = parsedData.email || '';
+  formElem.message.value = parsedData.message || '';
+
+  formData = parsedData;
+}
+
+formElem.addEventListener('input', e => {
+  if (e.target.name === 'email' || e.target.name === 'message') {
+    formData[e.target.name] = e.target.value.trim();
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }
+});
+
+formElem.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  formData = { email: '', message: '' };
+  formElem.reset();
+});
